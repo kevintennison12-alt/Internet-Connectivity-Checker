c49726abf55f060a2885902bdc3d900e1771ac4e
Kevin Tennison <kevintennison1@gmail.com>
2025-12-06 15:30:00 +0530

    Update check.php

--- check.php
+++ check.php
@@ -1,9 +1,16 @@
 <?php
-$url = $_GET["url"];
+header("Content-Type: application/json");
 
-$cmd = "python3 check_connectivity.py " . escapeshellarg($url);
+if (!isset($_POST["url"])) {
+    echo json_encode(["status" => "error", "message" => "No URL received"]);
+    exit;
+}
 
+$url = $_POST["url"];
+$url = escapeshellarg($url);
+
+$cmd = "python3 check_connectivity.py $url 2>&1";
 $output = shell_exec($cmd);
 
-echo $output; // Python script already returns JSON
+echo $output;
 ?>
